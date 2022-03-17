a49fb1c8f2441475bc7d6df51b4482921681a859
STamkovich <tamkovich1072@gmail.com>
2022-03-17 14:00:56 +0300

    "Конвертеры"

--- horoscope/views.py
+++ horoscope/views.py
@@ -1,52 +1,30 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-
-def aries(request):
-    return HttpResponse('Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).')
-
-
-def taurus(request):
-    return HttpResponse('Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).')
-
-
-def gemini(request):
-    return HttpResponse('Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).')
-
-
-def cancer(request):
-    return HttpResponse(' Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).')
-
-
-def leo(request):
-    return HttpResponse('Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).')
+zodiac_dict = {
+    'aries': 'Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).',
+    'taurus': 'Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).',
+    'gemini': 'Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).',
+    'cancer': 'Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).',
+    'leo': ' Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).',
+    'virgo': 'Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).',
+    'libra': 'Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
+    'scorpio': 'Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).',
+    'sagittarius': 'Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).',
+    'capricorn': 'Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).',
+    'aquarius': 'Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).',
+    'pisces': 'Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).',
+}
 
 
-def virgo(request):
-    return HttpResponse('Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).')
+def get_info_about_sign_zodiac(request, sign_zodiac):
+    description = zodiac_dict.get(sign_zodiac)
+    if description:
+        return HttpResponse(description)
+    else:
+        return HttpResponseNotFound(f'{sign_zodiac} такого знака зодиака не существует')
 
 
-def libra(request):
-    return HttpResponse('Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).')
-
-
-def scorpio(request):
-    return HttpResponse('Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).')
-
-
-def sagittarius(request):
-    return HttpResponse('Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).')
-
-
-def capricorn(request):
-    return HttpResponse('Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).')
-
-
-def aquarius(request):
-    return HttpResponse('Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).')
-
-
-def pisces(request):
-    return HttpResponse('Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).')
+def get_info_about_sign_zodiac_by_number(request, sign_zodiac):
+    return HttpResponse(f'This is number {sign_zodiac}')
